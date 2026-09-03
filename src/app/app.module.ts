9769f55e3083e9479fff02c5f2ab5c603c87ce9a
rarosales <rarosales@indracompany.com>
2026-09-02 21:40:55 -0500

    feat: [37] Implementa expandir/colapsar menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { NavComponent } from './shared/components/nav/nav.component';
 
+import { NavService } from './shared/services/nav.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +18,9 @@ import { NavComponent } from './shared/components/nav/nav.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    NavService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Subject, takeUntil } from "rxjs";
+
+import { NavService } from "../../services/nav.service";
 
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnInit, OnDestroy {
+
+    isOpen: boolean = true;
+
+    destroy$ = new Subject<void>();
+
+    constructor(private navService: NavService) {}
+
+    ngOnInit() {
+        this.subscribeOnOpen();
+    }
+
+    ngOnDestroy() {
+        this.destroy$.next();
+        this.destroy$.complete();
+    }
+
+    subscribeOnOpen() {
+        this.navService.onOpen.pipe(takeUntil(this.destroy$)).subscribe(() => {
+            this.isOpen = !this.isOpen;
+        });
+    }
 
 }
